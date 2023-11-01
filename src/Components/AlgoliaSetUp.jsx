9bfd7d3b22ed8b37ed445b4a954f10576a1c8411
DhaneshKJ <95269884+DhaneshKJ@users.noreply.github.com>
2023-11-01 23:51:51 +0530

    changes

--- src/Components/AlgoliaSetUp.jsx
+++ src/Components/AlgoliaSetUp.jsx
@@ -1,26 +1,31 @@
-
 import React, { useState } from "react";
 import './AlgoliaSetUp.css';
 import axios from 'axios';
 
 const AlgoliaSetUp = () => {
     const [applicationId, setApplicationId] = useState('');
-    const [indices, setIndices] = useState([]);
-  
+    const [indexName, setIndexName] = useState('');
+
     const addIndicesToIndicesArray = () => {
-      axios
-        .post('/v1/api/configuration/add-index', {
-          applicationId: applicationId,
-          indices: [indices],
-        })
-        .then((response) => {
-          // Handle the response as needed
-          console.log('Request successful', response.data);
-        })
-        .catch((error) => {
-          // Handle errors
-          console.error('Error:', error);
-        });
+        if (!applicationId || !indexName) {
+            alert("Application ID and Index Name are required.");
+            return;
+        }
+
+        axios
+            .post('/v1/api/algolia/bulk-index', {
+                applicationId: applicationId,
+                indexName: indexName,
+            })
+            .then((response) => {
+                console.log('Request successful', response.data);
+                alert('Indexed successful'); // Display a success alert
+                setApplicationId('');
+                setIndexName('');
+            })
+            .catch((error) => {
+                alert('Error:', error);
+            });
     };
 
     return (
@@ -42,8 +47,8 @@ const AlgoliaSetUp = () => {
                     <input
                         type="text"
                         placeholder="Enter your index name"
-                        value={indices}
-                        onChange={(e) => setIndices(e.target.value)}
+                        value={indexName}
+                        onChange={(e) => setIndexName(e.target.value)}
                         required
                     />
                 </div>
